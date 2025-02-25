7326c9da0c88957bd0fc3a58cd0a733be2b0491d
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-25 14:15:31 -0500

    Update AskInfo.py

--- AskInfo.py
+++ AskInfo.py
@@ -15,22 +15,23 @@ def main():
         return render_template('InputOutput.html')
     
 def GetInfo():
-    global password, username
-    username=request.form.get('txtusername')
-    password=request.form.get('txtpassword')
+{
+    global username, userpasswod;
+    print("I am GrtInfo");
+    useremail=request.form.get('txtusername')
+    userpassword=request.form.get('txtpassword')
     AddFile()
     print(username + password)
+}
 
 def AddFile():
     filename= username + "info.doc"
     pythfile = open(filename, "w")
     pythfile.write("Usrename:" + username + "\nPassword:" + password)
-
-def ReadFile():
-    pythfile = open(filename, "r")
-    info=file.readlines()
-    print(info)
     
+    FileConnectivity()
+    return render_template('frmOutput.html', username=useremail)
+
 if __name__ == "__main__":
     app.run()
  
