4ef698b7a4a38a5fd99a0b706d7c38893c53ed87
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-26 10:23:55 -0500

    Update AskInfo.py

--- AskInfo.py
+++ AskInfo.py
@@ -1,4 +1,3 @@
-import sys
 import os.path
 from os import path
 from flask import Flask, render_template, request
@@ -13,24 +12,23 @@ def main():
     else:
         GetInfo()
         return render_template('InputOutput.html')
-    
+
+@app.route('/info', methods=['GET', 'POST'])
 def GetInfo():
-{
-    global username, userpasswod;
-    print("I am GrtInfo");
+    global useremail, userpasswod;
     useremail=request.form.get('txtusername')
     userpassword=request.form.get('txtpassword')
-    AddFile()
-    print(username + password)
-}
-
-def AddFile():
-    filename= username + "info.doc"
-    pythfile = open(filename, "w")
-    pythfile.write("Usrename:" + username + "\nPassword:" + password)
     
+    print(useremail + userpasswod)
     FileConnectivity()
-    return render_template('frmOutput.html', username=useremail)
+    
+    return render_template('ouput.html', username=useremail, password=userpassword)
+
+def FileConnectivity():
+    filename = "userinfo.doc"
+    pythfile = open(filename, "w")
+    pythfile.write("User Email:" + useremail + "Password:" + userpasswod)
+    pythfile.close();
 
 if __name__ == "__main__":
     app.run()
